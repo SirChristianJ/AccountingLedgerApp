9ae1e7c4e1105c86f1b39fbe61686c4116d02625
SirChristianJ <git@github.com:SirChristianJ/todoApp.git>
2024-10-16 12:15:57 -0400

    All functionality working with prodecural solution, trying to implement object oriented solution using composition of account, user and transaction class

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -2,19 +2,22 @@ package com.pluralsight;
 
 import java.time.LocalDateTime;
 
-public class Transaction extends User{
+public class Transaction{
 
     private String date;
     private String time;
     private String description;
+    private String vendorName;
     private double amount;
+    private double runningBalance;
 
-    public Transaction(/*String userName, */ String date, String time, String description,String vendorName, double amount) {
-        super(/*userName,*/vendorName);
+    public Transaction( String date, String time, String description,String vendorName, double amount, double runningBalance) {
         this.date = date;
         this.time = time;
         this.description = description;
+        this.vendorName = vendorName;
         this.amount = amount;
+        this.runningBalance = runningBalance;
     }
 
     public String getDate() {
@@ -29,12 +32,21 @@ public class Transaction extends User{
         return description;
     }
 
+    public String getVendorName() {
+        return vendorName;
+    }
+
     public double getAmount() {
         return amount;
     }
 
+    public double getRunningBalance(){
+        return runningBalance;
+    }
+
     @Override
     public String toString() {
-        return String.format("%s|%s|%s|%s|%.2f"/*,getUserName()*/, date, time, description,getVendorName(), amount);
+        return String.format("%s|%s|%s|%s|%.2f|%.2f"/*,getUserName()*/, date, time, description,vendorName, amount, runningBalance);
     }
+
 }
